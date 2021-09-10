f86698833fe08e5ec59ce2c18fbf6a5ec5efa514
root <root@DESKTOP-HVCKQU5.localdomain>
2021-09-09 21:42:36 -0300

    repository refatoring

---
+++ airbnb-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Routes from './routes';
+
+const App = () => <Routes />;
+
+MapboxGL.setAccessToken('<aqui_vai_seu_token>');
+
+export default App;
---
+++ airbnb-app/src/services/api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+/* Endereços para cada emulador/simulador:
+** Genymotion:              http://10.0.3.2:3333/
+** Emulador Android Studio: http://10.0.2.2:3333/
+** Simulador IOS:           http://localhost:3333/
+*/
+const api = axios.create({
+  baseURL: 'http://localhost:3333/',
+});
+
+api.interceptors.request.use(async (config) => {
+  try {
+    const token = await AsyncStorage.getItem('@airbnb-app:token');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  } catch (err) {
+    alert(err);
+  }
+});
+
+export default api;
